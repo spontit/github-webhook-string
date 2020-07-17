c317cfdce283b06c6e26f0ad93c3c9062d4699ef
Josh Wolff <jw1@stanford.edu>
2020-07-17 10:22:05 -0600

    Initial commit. Adding basic webhook body function, example script, and example json from GitHub docs

---
+++ get_github_webhook_body.py
@@ -0,0 +1,22 @@
+def get_github_body_str(payload):
+    result = ""
+    if payload.get('action'):
+        result += 'Action: ' + str(payload['action']) + "\n"
+    if payload.get('comment'):
+        result += 'Comment: ' + str(payload['comment']) + "\n"
+    if payload.get('issue'):
+        issue = payload['issue']
+        issue_number = issue.get('number')
+        issue_title = issue.get('title')
+        issue_sender = issue.get('user', dict()).get('login')
+        if issue_number and issue_title:
+            result += 'New Issue #' + str(issue_number) + ': ' + str(issue_title) + '\n'
+        if issue_sender:
+            result += "Issue Submitted By: " + str(issue_sender) + '\n'
+    if payload.get('repository'):
+        if payload.get('name'):
+            result += 'Repository ' + str(payload['repository']['name']) + '\n'
+    if payload.get('sender'):
+        if payload.get('login'):
+            result += "By: " + payload['sender']['login']
+    return result
